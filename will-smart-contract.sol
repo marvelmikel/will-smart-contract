5af6964fb1d6bd9531b00db00e292102b8285a88
marvelmikel <marvelmikel1997@gmail.com>
2022-08-18 15:41:22 -0700

    will contract

---
+++ will-smart-contract.sol
@@ -0,0 +1,66 @@
+pragma  solidity ^0.5.7;
+
+contract Will{
+    address owner;
+    uint fortune;
+    bool deceased;
+
+    constructor() payable public{
+    owner = msg.sender; // msg sender represents address that is being called
+    fortune = msg.value; // msg value tells us how much ether is being sent
+    deceased = false;
+
+
+    }
+
+    // create modifier so the  only person who can only call the contract is the owner
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    //create modifier so that we only allocate  funds if  freinds gramps deceased
+     modifier mustBeDeceased {
+        require(deceased == true);
+        _;
+    }
+    
+    //list of family wallets
+    address payable[] familyWallets;
+    
+    // map through inheritance
+    mapping(address => uint) inheritance;
+
+    // set inheritance for each address
+    function setInheritance(address payable wallet, uint amount) public onlyOwner{
+        // to add walet to the family wallet . push
+
+        familyWallets.push(wallet);
+        inheritance[wallet] = amount;
+
+    }
+
+    //pay each family member base on their wallet address
+
+    function payout() private mustBeDeceased {
+        // with a for loop you can loop through things and set conditions 
+
+        for(uint i=0; i<familyWallets.length; i++) {
+            familyWallets[i].transfer(inheritance[familyWallets[i]]);
+            //transfering the funds from contract address to reciever address
+
+        }
+
+    }
+    
+    //oracle switch simulation
+    function hasDeceased() public onlyOwner {
+        deceased = true;
+        payout();
+    }
+
+
+
+
+}
